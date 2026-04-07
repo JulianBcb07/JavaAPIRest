081550592668d88c27c085501ff350bbf9204cb1
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2026-04-06 23:43:06 -0600

    feat: implement update and soft delete for doctors
    - Create DatosEditarMedico DTO for controlled updates.
    - Implement soft delete logic using a new field in database.
    - Update Medico entity to handle record status.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListaMedico;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,7 +26,22 @@ public class MedicoController {
 
     @GetMapping
     public Page<DatosListaMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nombre"}) Pageable paginacion) {
-        return repository.findAll(paginacion).map(DatosListaMedico::new);
+        return repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void actualizar(@RequestBody @Valid DatosEditarMedico datos) {
+
+        var medico = repository.getReferenceById(datos.id());
+        medico.actualizarDatos(datos);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void eliminar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.eliminar();
     }
 
 }
