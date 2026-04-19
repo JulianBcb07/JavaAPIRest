245ad14f02693a8bbcf3ba6711f3416f7bcf188b
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2026-04-18 19:11:18 -0600

    feat: improve HTTP responses in PacienteController using ResponseEntity
    - Implement ResponseEntity in all patient endpoints for proper status code.
    - Return '201 Created' and location header on succesfull registration.
    - Return '204 No Content' for soft deletes.
    - Standardize API responses using DTOs for data projection.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -22,20 +22,35 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void registrar(@RequestBody @Valid DatosRegistroPaciente datos) {
-        repository.save(new Paciente(datos));
+    public ResponseEntity registrar(@RequestBody @Valid DatosRegistroPaciente datos, UriComponentsBuilder uriComponentsBuilder) {
+
+        var paciente = new Paciente(datos);
+        repository.save(paciente);
+
+        var uri = uriComponentsBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DatosDetallePaciente(paciente));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity obtenerPaciente(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DatosDetallePaciente(paciente));
     }
 
     @GetMapping
-    public Page<DatosListaPaciente> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion) {
-        return repository.findByActivoTrue(paginacion).map(DatosListaPaciente::new);
+    public ResponseEntity<Page<DatosListaPaciente>> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion) {
+        var page = repository.findByActivoTrue(paginacion).map(DatosListaPaciente::new);
+
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void actualizar(@RequestBody @Valid DatosEditarPaciente datos) {
+    public ResponseEntity actualizar(@RequestBody @Valid DatosEditarPaciente datos) {
         var paciente = repository.getReferenceById(datos.id());
         paciente.actualizarDatos(datos);
+
+        return ResponseEntity.ok(new DatosDetallePaciente(paciente));
     }
 
     @DeleteMapping("/{id}")
---
+++ src/main/java/med/voll/api/paciente/DatosDetallePaciente.java
@@ -0,0 +1,25 @@
+package med.voll.api.paciente;
+
+import med.voll.api.direccion.Direccion;
+
+public record DatosDetallePaciente(
+        Long id,
+        String nombre,
+        String email,
+        String telefono,
+        String documento_identidad,
+        Direccion direccion
+) {
+
+    public DatosDetallePaciente(Paciente paciente) {
+        this(
+                paciente.getId(),
+                paciente.getNombre(),
+                paciente.getEmail(),
+                paciente.getTelefono(),
+                paciente.getDocumentoIdentidad(),
+                paciente.getDireccion()
+                );
+    }
+
+}
