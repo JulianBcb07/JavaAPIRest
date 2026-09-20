0723d3512c7f2307609fb004dd31b274be048a5d
JulianBcb07 <julianbacab.dc7@gmail.com>
2026-09-20 14:27:00 -0600

    test: add controller and repository integration test
    - Add application-test.properties for test environment setup
    - Add ConsultaControllerTest for endpoint validation
    - Add MedicoRepositoryTest for database queries validation

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,4 +1,120 @@
-import static org.junit.jupiter.api.Assertions.*;
+package med.voll.api.domain.medico;
+
+import jakarta.persistence.EntityManager;
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.direccion.DatosDireccion;
+import med.voll.api.domain.paciente.DatosRegistroPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class MedicoRepositoryTest {
-  
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private EntityManager entityManager; // Trae todos los repositories necesarios para obtener la informacion
+
+    @Test
+    @DisplayName("Deberia devolver null cuando el medico buscado existe pero no esta disponble en esa fecha")
+    void elegirMedicoAleatorioDisponibleEnLaFechaEscenario1() {
+        // given o arrange
+        // Se cambia la fecha de ahora por el lunes siguiente a las 10 a.m.
+        var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
+        var medico = registrarMedico("Medico1", "Medico@gmail.com", "12345", Especialidad.CARDIOLOGIA);
+        var paciente = registrarPaciente("Paciente1", "Paciente@gmail.com", "12345678");
+        registrarConsulta(medico, paciente, lunesSiguienteALas10);
+
+        // when or act
+        var medicoLibre = medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
+
+        // then or assert
+        assertThat(medicoLibre).isNull();
+
+    }
+
+    @Test
+    @DisplayName("Deberia devolver medico cuando el medico buscado esta disponible en esa fecha")
+    void elegirMedicoAleatorioDisponibleEnLaFechaEscenario2() {
+        // give or arrange
+        // Se cambia la fecha de ahora por el lunes siguiente a las 10 a.m.
+        var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
+        var medico = registrarMedico("Medico1", "Medico@gmail.com", "12345", Especialidad.CARDIOLOGIA);
+
+        // when or act
+        var medicoLibre = medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
+
+        // then or assert
+        assertThat(medicoLibre).isEqualTo(medico);
+    }
+
+    private void registrarConsulta(Medico medico, Paciente paciente, LocalDateTime fecha) {
+        entityManager.persist(new Consulta(null, medico, paciente, fecha));
+    }
+
+    private Medico registrarMedico(String nombre, String email, String documento, Especialidad especialidad) {
+        var medico = new Medico(datosMedico(nombre, email, documento, especialidad));
+        entityManager.persist(medico);
+        return medico;
+    }
+
+    private Paciente registrarPaciente(String nombre, String email, String documento) {
+        var paciente = new Paciente(datosPaciente(nombre, email, documento));
+        entityManager.persist(paciente);
+        return paciente;
+    }
+
+    private DatosRegistroMedico datosMedico(String nombre, String email, String documento, Especialidad especialidad) {
+        return new DatosRegistroMedico(
+                nombre,
+                email,
+                "9869593933",
+                documento,
+                especialidad,
+                datosDireccion()
+        );
+    }
+
+    private DatosRegistroPaciente datosPaciente(String nombre, String email, String documento) {
+        return new DatosRegistroPaciente(
+                nombre,
+                email,
+                "9585839242",
+                documento,
+                datosDireccion()
+        );
+    }
+
+    private DatosDireccion datosDireccion() {
+        return new DatosDireccion(
+                "calle x",
+                "distrito y",
+                "434",
+                "123",
+                "valladolid",
+                "97782",
+                "yucatan"
+        );
+    }
+
+
+
+
+
+
 }
